3fde89a69fb182a7f9377a74236cf4381de0588a
vagrant <vagrant@student-dev-box>
2014-03-06 21:13:43 +0000

    Created all of the tests for the pizza priject.

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -1,13 +1,17 @@
 require './pizza'
 
 describe Pizza do
+  describe '.initialize' do
   it 'records all of the toppings' do 
     toppings = [
-      Topping.new('mushrooms', vegitarian: true), Topping.new('pepperoni')]
-      pizza = Pizza.new(toppings)
+      Topping.new('mushrooms', vegetarian: true), 
+      Topping.new('pepperoni')
+    ]
+      pizza = Pizza.new(toppings: toppings)
 
       expect(pizza.toppings).to eq(toppings)
-    end
+  end
+
 
     it 'defaults the toppings to cheese only, if the pizza has no toppings' do
       pizza = Pizza.new
@@ -15,29 +19,31 @@ describe Pizza do
       expect(pizza.toppings.size).to  eq(1)
       expect(pizza.toppings.first.name).to eq('cheese')
     end
-
-  it "exists" do
-    expect(Pizza).to be_a(Class)
   end
 end
 
 describe Topping do
-  describe '.initialize' do
-    it 'sets the name of the topping' do
-      topping = Topping.new('olives')
-
-      expect(topping.name).to eq('olives')
-    end
-
-      it 'sets wheter or not the topping is vegitarian' do
-        topping = Topping.new 'bell peppers', vegitarian: true
-
-        expect(topping.vegitarian).to eq(true)
-      end
-    end
- 
 
   it "exists" do
-    expect(Topping).to be_a(Class)
+    expect(Pizza).to be_a(Class)
   end
-end
+
+  describe '.initialize' do
+    it 'sets the name of the topping' do
+      topping = Topping.new('pepperoni')
+      expect(topping.name).to eq('pepperoni')
+    end
+
+      it 'sets wheter or not the topping is vegetarian' do
+        topping = Topping.new 'bell peppers', vegetarian: true
+
+        expect(topping.vegetarian).to eq(true)
+      end
+    end
+  end
+ 
+
+  #it "exists" do
+   # expect(Topping).to be_a(Class)
+ # end
+#end
